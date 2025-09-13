087e54b28ecd1f9b2795f5ce19db2ca6a2fc7323
SerVin <v.s.work@yandex.ru>
2025-09-13 14:49:44 +0300

    --base-concepts completed--

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,41 @@
-"use strict"
+"use strict";
+
+// base-concepts__1
+
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = b**2 - 4 * a * c;
+
+  let i1,
+      i2;
+
+  if (d > 0){
+    i1 = (-b + Math.sqrt(d) ) / (2 * a);
+    i2 = (-b - Math.sqrt(d) ) / (2 * a);
+    arr.push(i1, i2);
+  } else if (d === 0) {
+    i1 = -b / (2 * a);
+    arr = [i1];
   
   return arr;
 }
+}
+
+
+console.log(solveEquation());
+
+// base-concepts__2
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+ let p, s, n, mp, SUM;
+
+ n = countMonths;
+ p = +(percent / 100 / 12);
+
+ s = +(amount - contribution);
+ mp = +(s * (p + (p / (((1 + p) ** n) - 1))));
+
+ SUM = +((mp * n).toFixed(2));
+ return SUM;
 }
+  
