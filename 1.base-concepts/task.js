542d2730453e6f44956b2b6454ee13c505a727e4
SerVin <v.s.work@yandex.ru>
2025-09-13 14:11:05 +0300

    commit init

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,33 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+"use strict";
+
+function solveEquation(a, b, с) {
+	let arr = [];
+	let d = b ** 2 - 4 * a * с;
+
+	let i1,
+		i2;
+
+	if (d > 0) {
+		i1 = (-b + Math.sqrt(d)) / (2 * a);
+		i2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(i1, i2);
+	} else if (d === 0) {
+		i1 = -b / (2 * a);
+		arr.push(i1);
+	}
+	return arr;
 }
+console.log(solveEquation());
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+ let p, s, n, mp, SUM;
+
+ n = countMonths;
+ p = +(percent / 100 / 12);
+
+ s = +(amount - contribution);
+ mp = +(s * (p + (p / (((1 + p) ** n) - 1))));
+
+ SUM = +((mp * n).toFixed(2));
+ return SUM;
 }
